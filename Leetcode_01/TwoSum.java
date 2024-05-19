3d56e3270d830c5a9e2f139aee0db816356dd91f
Omair Iqbal <Omair786955@gmail.com>
2024-05-19 23:17:18 +0100

    Leetcode 1: Two Sum

---
+++ Leetcode_01/TwoSum.java
@@ -0,0 +1,19 @@
+package Leetcode_01;
+
+import java.util.HashMap;
+
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> res = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (res.containsKey(target - nums[i])) {
+                return new int [] {i, res.get(target - nums[i])};
+            }
+            res.put(nums[i], i);
+        }
+
+        return new int [] {};
+    }
+}
